d1ea3acd36499d73f00384f4bff2ec0655bcf980
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-10-14 09:29:38 -0400

    mongoose server setup and running - routes need work

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please enter a valid email address.",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
+
+// export the User model
+module.exports = User;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +13,6 @@ app.use(require("./routes"));
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
   {
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
